4b9141c3884f085f8e46bd50fb264e949fbe8662
marcomarchesi <marchesimarco@gmail.com>
2017-11-09 20:35:00 +0000

    tested training

--- run_test_mac.sh
+++ run_test_mac.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-python evaluate.py --checkpoint '/Users/marcomarchesi/Desktop/fast-style-transfer/checkpoints/scream.ckpt' \
+python evaluate.py --checkpoint '/Users/marcomarchesi/Desktop/fast-style-transfer/checkpoint/fns.ckpt' \
   --in-path '/Users/marcomarchesi/Desktop/fast-style-transfer/input' \
   --out-path '/Users/marcomarchesi/Desktop/fast-style-transfer/output' \
   --allow-different-dimensions
--- run_train_mac.sh
+++ run_train_mac.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 python style.py --style '/Users/marcomarchesi/Desktop/fast-style-transfer/style/bailey_01.jpg' \
-    --checkpoint-dir '/Users/marcomarchesi/Desktop/fast-style-transfer/checkpoints' \
+    --checkpoint-dir '/Users/marcomarchesi/Desktop/fast-style-transfer/checkpoint' \
     --test '/Users/marcomarchesi/Desktop/fast-style-transfer/input/giraffe.jpg' \
     --test-dir '/Users/marcomarchesi/Desktop/fast-style-transfer/input/' \
     --content-weight 1.5e1 \
-    --checkpoint-iterations 1000 \
+    --checkpoint-iterations 2 \
     --batch-size 1
