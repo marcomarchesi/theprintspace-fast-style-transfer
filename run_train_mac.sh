0670e24c9cf5b805a62c6f68bbcb34c03917c749
marcomarchesi <marchesimarco@gmail.com>
2017-11-26 22:59:01 +0000

    testing affine 0.0

--- run_train_mac.sh
+++ run_train_mac.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 python style.py --style-dir '/Users/marcomarchesi/Desktop/fast-style-transfer/style' \
-    --checkpoint-dir '/Users/marcomarchesi/Desktop/fast-style-transfer/checkpoint' \
+    --checkpoint-dir '/Users/marcomarchesi/Desktop/fast-style-transfer/checkpoint/ckpt_03' \
     --test '/Users/marcomarchesi/Desktop/fast-style-transfer/input/giraffe.jpg' \
     --test-dir '/Users/marcomarchesi/Desktop/fast-style-transfer/test/' \
     --content-weight 1.5e1 \
     --checkpoint-iterations 2 \
-    --batch-size 5 \
+    --batch-size 20 \
     --epochs 2 \
     --no-gpu \
     --num-examples 10 \
-    --affine 
+    --affine-weight 0.0 \
+    --affine
--- src/optimize.py
+++ src/optimize.py
@@ -42,7 +42,7 @@ def get_affine_loss(output, batch_size, MM, weight):
 def optimize(content_targets, style_targets, content_weight, style_weight,
              tv_weight, affine_weight, vgg_path, epochs=2, print_iterations=1,
              batch_size=4, save_path='saver/fns.ckpt', slow=False,
-             learning_rate=1e-3, debug=False, no_gpu=False, affine=False, num_examples=1000):
+             learning_rate=1e-3, debug=False, no_gpu=False, affine=False, multiple_style_images=False, num_examples=1000):
 
 
     DEVICES = '/gpu:0'
@@ -181,7 +181,7 @@ def optimize(content_targets, style_targets, content_weight, style_weight,
 
         if affine:
             saver = tf.train.Saver()
-            #saver.restore(sess, save_path)
+            saver.restore(sess, save_path)
             #print("checkpoint restored")
 
 
